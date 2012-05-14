204720221c82ed3c5fc81b1c2a1fef9c291217b9
Atsuo Ishimoto <ishimoto@tpx200s.(none)>
2012-05-15 00:02:32 +0900

    Supports blueprint

--- demo_pkgrsrc/demo.py
+++ demo_pkgrsrc/demo.py
@@ -1,26 +1,43 @@
 import os
 import pkg_resources
-from flask import Flask, helpers, render_template
+from flask import Flask, helpers, render_template, Blueprint
 from jinja2 import loaders
 
-class ZippedFlask(Flask):
+class ResourceMixin(object):
     PACKAGENAME = "demo_pkgrsrc"
-    static_resource = "static"
     
     @helpers.locked_cached_property
     def jinja_loader(self):
-        return loaders.PackageLoader(self.PACKAGENAME)
+        return loaders.PackageLoader(self.PACKAGENAME, self.template_folder)
 
     def send_static_file(self, filename):
-        fname = helpers.safe_join(self.static_resource, filename)
+        fname = helpers.safe_join(self._static_folder, filename)
         f = pkg_resources.resource_stream(self.PACKAGENAME, fname)
         return helpers.send_file(f, attachment_filename=filename)
+
+
+class ZippedFlask(ResourceMixin, Flask):
+    pass
+
+
+class ZippedBlueprint(ResourceMixin, Blueprint):
+    pass
+
             
 app = ZippedFlask(__name__)
+
+blueprint = ZippedBlueprint('blueprint_test', __name__, template_folder='templates2', static_folder='blueprint_static', static_url_path='/static2') 
 
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+
+@blueprint.route('/blueprint/')
+@blueprint.route('/blueprint/<name>')
+def hello2(name=''):
+    return render_template('hello2.html', name=name+" blueprint")
+
+app.register_blueprint(blueprint)
 
 if __name__ == "__main__":
     app.run()
